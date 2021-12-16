fd95fc2ae06ce1f8bbc944f037b4fd92a86801f5
shouao <792417939@qq.com>
2021-12-16 17:52:17 +0800

    add

--- deque/deque.cpp
+++ deque/deque.cpp
@@ -3,6 +3,9 @@
 template<typename T>
 class deque {
   public:
+    static const size_t buffer_size = 1024;
+    class deque_buffer;
+	typedef deque_buffer* buffer_ptr;
 	class const_iterator;
 	class iterator {
     private:
@@ -10,89 +13,228 @@ class deque {
 		 * TODO add data members
 		 *   just add whatever you want.
 		 */
+        buffer_ptr b_ptr;
+        T* e_ptr;
 	public:
 		/**
 		 * return a new iterator which pointer n-next elements
 		 *   even if there are not enough elements, the behaviour is **undefined**.
 		 * as well as operator-
 		 */
+        iterator(buffer_ptr __b_ptr, T* __e_ptr):b_ptr(__b_ptr), e_ptr(__e_ptr) {}
+        iterator(const const_iterator &other):b_ptr(other.b_ptr), e_ptr(other.e_ptr) {}
+        iterator(const iterator &other):b_ptr(other.b_ptr), e_ptr(other.e_ptr) {}
 		iterator operator+(const int &n) const {
 			//TODO
+            b_ptr += n / buffer_size;
+            e_ptr += n % buffer_size;
+            if (e_ptr >= buffer_size) {
+                ++b_ptr;
+                e_ptr -= buffer_size;
+            }
 		}
 		iterator operator-(const int &n) const {
 			//TODO
+            b_ptr -= n / buffer_size;
+            e_ptr -= n % buffer_size;
+            if (e_ptr < 0) {
+                --b_ptr;
+                e_ptr += buffer_size;
+            }
 		}
 		// return th distance between two iterator,
 		// if these two iterators points to different vectors, throw invaild_iterator.
 		int operator-(const iterator &rhs) const {
 			//TODO
+			return (rhs.b_ptr - b_ptr)*buffer_size + rhs.e_ptr - e_ptr;
 		}
 		iterator operator+=(const int &n) {
 			//TODO
+			iterator result = *this;
+			*this = (*this) + n;
+			return result;
 		}
 		iterator operator-=(const int &n) {
 			//TODO
+			iterator result = *this;
+			*this = (*this) - n;
+			return result;
 		}
 		/**
 		 * TODO iter++
 		 */
-		iterator operator++(int) {}
+		iterator operator++(int) {
+		    (*this) = (*this) + 1;
+            return (*this);
+		}
 		/**
 		 * TODO ++iter
 		 */
-		iterator& operator++() {}
+		iterator& operator++() {
+            return (*this) + 1;
+		}
 		/**
 		 * TODO iter--
 		 */
-		iterator operator--(int) {}
+		iterator operator--(int) {
+            (*this) = (*this) - 1;
+            return (*this);
+		}
 		/**
 		 * TODO --iter
 		 */
-		iterator& operator--() {}
+		iterator& operator--() {
+            return (*this) - 1;
+		}
 		/**
 		 * TODO *it
 		 */
-		T& operator*() const {}
+		T& operator*() const {
+            return *(e_ptr);
+		}
 		/**
 		 * TODO it->field
 		 */
-		T* operator->() const noexcept {}
+		T* operator->() const noexcept {
+            return e_ptr;
+		}
 		/**
 		 * a operator to check whether two iterators are same (pointing to the same memory).
 		 */
-		bool operator==(const iterator &rhs) const {}
-		bool operator==(const const_iterator &rhs) const {}
+		bool operator==(const iterator &rhs) const {
+            return e_ptr == rhs.eptr && b_ptr == rhs.b_ptr;
+		}
+		bool operator==(const const_iterator &rhs) const {
+            return e_ptr == rhs.eptr && b_ptr == rhs.b_ptr;
+		}
 		/**
 		 * some other operator for iterator.
 		 */
-		bool operator!=(const iterator &rhs) const {}
-		bool operator!=(const const_iterator &rhs) const {}
+		bool operator!=(const iterator &rhs) const {
+            return !(*this == rhs);
+		}
+		bool operator!=(const const_iterator &rhs) const {
+            return !(*this == rhs);
+		}
 	};
 	class const_iterator {
 		// it should has similar member method as iterator.
 		//  and it should be able to construct from an iterator.
 		private:
-			// data members.
-		public:
-			const_iterator() {
-				// TODO
-			}
-			const_iterator(const const_iterator &other) {
-				// TODO
-			}
-			const_iterator(const iterator &other) {
-				// TODO
-			}
-			// And other methods in iterator.
-			// And other methods in iterator.
-			// And other methods in iterator.
+		/**
+		 * TODO add data members
+		 *   just add whatever you want.
+		 */
+        buffer_ptr b_ptr;
+        T* e_ptr;
+	public:
+		/**
+		 * return a new iterator which pointer n-next elements
+		 *   even if there are not enough elements, the behaviour is **undefined**.
+		 * as well as operator-
+		 */
+        const_iterator(buffer_ptr __b_ptr, T* __e_ptr):b_ptr(__b_ptr), e_ptr(__e_ptr) {}
+        const_iterator(const const_iterator &other):b_ptr(other.b_ptr), e_ptr(other.e_ptr) {}
+        const_iterator(const iterator &other):b_ptr(other.b_ptr), e_ptr(other.e_ptr) {}
+
+		const_iterator operator+(const int &n) const {
+			//TODO
+            b_ptr += n / buffer_size;
+            e_ptr += n % buffer_size;
+            if (e_ptr >= buffer_size) {
+                ++b_ptr;
+                e_ptr -= buffer_size;
+            }
+		}
+		const_iterator operator-(const int &n) const {
+			//TODO
+            b_ptr -= n / buffer_size;
+            e_ptr -= n % buffer_size;
+            if (e_ptr < 0) {
+                --b_ptr;
+                e_ptr += buffer_size;
+            }
+		}
+		// return th distance between two iterator,
+		// if these two iterators points to different vectors, throw invaild_iterator.
+		int operator-(const const_iterator &rhs) const {
+			//TODO
+			return (rhs.b_ptr - b_ptr)*buffer_size + rhs.e_ptr - e_ptr;
+		}
+		const_iterator operator+=(const int &n) {
+			//TODO
+			const_iterator result = *this;
+			*this = (*this) + n;
+			return result;
+		}
+		const_iterator operator-=(const int &n) {
+			//TODO
+			const_iterator result = *this;
+			*this = (*this) - n;
+			return result;
+		}
+		/**
+		 * TODO iter++
+		 */
+		const_iterator operator++(int) {
+		    (*this) = (*this) + 1;
+            return (*this);
+		}
+		/**
+		 * TODO ++iter
+		 */
+		const_iterator& operator++() {
+            return (*this) + 1;
+		}
+		/**
+		 * TODO iter--
+		 */
+		const_iterator operator--(int) {
+            (*this) = (*this) - 1;
+            return (*this);
+		}
+		/**
+		 * TODO --iter
+		 */
+		const_iterator& operator--() {
+            return (*this) - 1;
+		}
+		/**
+		 * TODO *it
+		 */
+		T& operator*() const {
+            return *(e_ptr);
+		}
+		/**
+		 * TODO it->field
+		 */
+		T* operator->() const noexcept {
+            return e_ptr;
+		}
+		/**
+		 * a operator to check whether two iterators are same (pointing to the same memory).
+		 */
+		bool operator==(const iterator &rhs) const {
+            return e_ptr == rhs.eptr && b_ptr == rhs.b_ptr;
+		}
+		bool operator==(const const_iterator &rhs) const {
+            return e_ptr == rhs.eptr && b_ptr == rhs.b_ptr;
+		}
+		/**
+		 * some other operator for iterator.
+		 */
+		bool operator!=(const iterator &rhs) const {
+            return !(*this == rhs);
+		}
+		bool operator!=(const const_iterator &rhs) const {
+            return !(*this == rhs);
+		}
 	};
 	class deque_buffer{
 	    public:
-          static const size_t buffer_size = 1024;
           T* data;
           int ptr;
-          deque_buffer():ptr(0){
+          deque_buffer(int _ptr = 0):ptr(_ptr){
               data = (T*)malloc(buffer_size * sizeof(T));
           }
           deque_buffer(deque_buffer& other) {
@@ -101,21 +243,32 @@ class deque {
           ~deque_buffer(){
               free(data);
           }
-          void reverse() {
-              for (int i = 0; i < (buffer_size>>1); ++i) {
-                  swap(data[i], data[buffer_size - 1 - i]);
-              }
+          bool push_back(const T& x) {
+             data[ptr++] = x;
+             return (ptr == buffer_size);
+          }
+          bool pop_back() {
+            --ptr;
+            return (ptr == -1);
+          }
+          bool push_front(const T& x) {
+             data[--ptr] = x;
+             return (ptr == 0);
+          }
+          bool pop_front() {
+            ++ptr;
+            return (ptr == buffer_size);
           }
 	};
-	typedef deque_buffer* buffer_ptr;
 
 	buffer_ptr buffer_head;
     buffer_ptr buffer_tail;
     size_t  buffer_num;
+
 	/**
 	 * TODO Constructors
 	 */
-	deque() :(){}
+	deque():buffer_num(0), buffer_head(0), buffer_tail(0)  {}
 	deque(const deque &other) {
         buffer_num = other.buffer_num;
         buffer_head = (buffer_ptr)malloc(buffer_num * sizeof(T*));
